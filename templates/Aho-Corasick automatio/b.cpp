c41d8f002cc2618336cfbf5b06f16d98ece91a2a
Deng Yucheng <corndeng@hotmail.com>
2019-09-26 21:00:18 +0800

    更新AC自动机模板

---
+++ templates/Aho-Corasick automatio/b.cpp
@@ -0,0 +1,88 @@
+//此版本保留trie树原有结构
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <queue>
+using namespace std;
+
+#define maxn 1000010
+
+struct node {
+    int fail; //失配指针
+    int son[26];//子节点编号
+    bool sone[26]; //记录子节点是否存在
+    int ends; //结束标记
+} trie[maxn];
+
+int cnt = 0; //当前结点个数
+int newnode() { //新节点初始化
+    trie[++cnt].fail = 0;
+    trie[cnt].ends = 0;
+    for(int i = 0; i < 26; i++) 
+        trie[cnt].son[i] = trie[cnt].sone[i] = 0;
+    return cnt;
+}
+void build(char *s) { //构建trie树
+    int len = strlen(s), now = 0;
+    for(int i = 0; i < len; i++) {
+        if(trie[now].son[s[i]-'a'] == 0) {
+            trie[now].son[s[i]-'a'] = newnode();
+            trie[now].sone[s[i]-'a'] = 1;
+        }
+        now = trie[now].son[s[i]-'a'];
+    }
+    trie[now].ends++;
+}
+
+void get_fail() { //计算失配指针（构建trie图）
+    queue<int> que;
+    int now, last;
+    for(int i = 0; i < 26; i++)
+        if(trie[0].son[i]) {
+            trie[trie[0].son[i]].fail = 0;
+            que.push(trie[0].son[i]);
+        }
+    while(!que.empty()) {
+        now = que.front(); que.pop();
+        last = trie[now].fail;
+        for(int i = 0; i < 26; i++)
+            if (trie[now].son[i]) {
+                while(!trie[last].son[i] && last) last = trie[last].fail;
+                trie[trie[now].son[i]].fail = trie[last].son[i];
+                que.push(trie[now].son[i]);
+            }    
+    }
+}
+
+int work(char *s) { //匹配，计算串s中存在的模式串数量
+    int len = strlen(s), now = 0, ans = 0;
+    for(int i = 0; i < len; i++) {
+        now = trie[now].son[s[i]-'a'];
+        for(int t = now; t!=0&&trie[t].ends>=0; t=trie[t].fail) {
+            ans += trie[t].ends;
+            trie[t].ends = -trie[t].ends-1;
+        }
+    }
+    return ans;
+}
+
+void repair() { //恢复ends
+    for(int i = 0; i <= cnt; i++)
+        if(trie[i].ends < 0)
+            trie[i].ends = -trie[i].ends-1;
+}
+
+char str[maxn];
+int main() {
+    int n;
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++) {
+        scanf("%s", str);
+        build(str);
+    }
+    get_fail();
+    scanf("%s", str);
+    printf("%d\n", work(str));
+    //repair();
+    return 0;
+}
